7571a1eba0913694329384fd1f2ef0d3308087c1
Kinfe123 <kinfishtech@gmail.com>
2024-02-01 11:24:02 +0300

    adding some docs

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -39,11 +39,11 @@ export default async function DocsLayout({ children }: DocsLayoutProps) {
   const user = await getCurrentUser()
 
   return (
-    <div className="flex h-screen overflow-x-hidden overflow-clip flex-col">
+    <div className="flex h-screen  overflow-y-auto flex-col">
       <NavBar user={user} items={docsConfig.mainNav} rightElements={rightHeader()}>
         <DocsSidebarNav items={docsConfig.sidebarNav} />
       </NavBar>
-      <div className="container flex-1  overflow-x-hidden">{children}</div>
+      <div className="container flex-1  overflow-hidden ">{children}</div>
       <SiteFooter className="border-t" />
     </div>
   )
