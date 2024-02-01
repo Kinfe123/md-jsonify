f1ede453a552a7e7bb7eb57de3f7987e3f588742
Kinfe123 <kinfishtech@gmail.com>
2024-02-01 17:12:30 +0300

    tip fix

--- components/gradient-btn.tsx
+++ components/gradient-btn.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 const GetStarted = () => {
     return (
-      <button className='relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
+      <button className='relative inline-flex h-12 overflow-hidden rounded-2xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
         <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
-        <span className='text-md inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-slate-950 px-4 py-2 font-medium text-white backdrop-blur-3xl'>
+        <span className='text-md inline-flex h-full w-full cursor-pointer items-center justify-center rounded-2xl bg-slate-950 px-4 py-2 font-medium text-white backdrop-blur-3xl'>
          <Link href={'/docs'}>
           Get Started
          </Link>
