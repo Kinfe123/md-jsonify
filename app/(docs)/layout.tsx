7347fd10f5b6a5737160f4cab5e4c2740f4b66c6
Kinfe123 <kinfishtech@gmail.com>
2024-02-01 11:43:54 +0300

    update feeder

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -39,11 +39,11 @@ export default async function DocsLayout({ children }: DocsLayoutProps) {
   const user = await getCurrentUser()
 
   return (
-    <div className="flex h-screen  overflow-y-auto flex-col">
+    <div className="flex h-screen  flex-col">
       <NavBar user={user} items={docsConfig.mainNav} rightElements={rightHeader()}>
         <DocsSidebarNav items={docsConfig.sidebarNav} />
       </NavBar>
-      <div className="container flex-1  overflow-hidden ">{children}</div>
+      <div className="container flex-1  ">{children}</div>
       <SiteFooter className="border-t" />
     </div>
   )
