d7b24dae2bf58d732516c5dcd4bb05ba861b8cbb
Kinfe123 <kinfishtech@gmail.com>
2024-02-04 18:23:06 +0300

    playground init

---
+++ app/(marketing)/playground/page.tsx
@@ -0,0 +1,11 @@
+import PlaygoundComp from "@/components/playground-comp/playground"
+
+async function Playground() {
+    return (
+        <div className="h-screen">
+            <PlaygoundComp />
+        </div>
+
+    )
+}
+export default Playground   
