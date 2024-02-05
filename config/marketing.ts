8123258d5c75068841bd416b21e5b413214bb6fd
Kinfe123 <kinfishtech@gmail.com>
2024-02-05 23:14:07 +0300

    added page

--- config/marketing.ts
+++ config/marketing.ts
@@ -2,13 +2,14 @@ import { MarketingConfig } from "types"
 
 export const marketingConfig: MarketingConfig = {
   mainNav: [
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
+  
     {
       title: "Changelog",
       href: "/changelog",
+    },
+    {
+      title: "Documentation",
+      href: "/docs",
     },
   ],
 }
