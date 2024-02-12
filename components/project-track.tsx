b6bfc93e0357a4af4e812fe3a6688e64c375ef81
Kinfe123 <kinfishtech@gmail.com>
2024-02-12 18:13:00 +0300

    fix: content updated

--- components/project-track.tsx
+++ components/project-track.tsx
@@ -295,30 +295,33 @@ export const Board = () => {
   
   const DEFAULT_CARDS = [
     // BACKLOG
-    { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
-    { title: "SOX compliance checklist", id: "2", column: "backlog" },
-    { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
-    { title: "Document Notifications service", id: "4", column: "backlog" },
+    { title: "More Curated Parsing of the data", id: "1", column: "backlog" },
+    { title: "Somehow integrated with AI to make it pass the edgecases", id: "2", column: "backlog" },
+    { title: "Optimizing some utils functions", id: "3", column: "backlog" },
+    { title: "Making sure it is consitent accross the api", id: "4", column: "backlog" },
     // TODO
     {
-      title: "Research DB options for new microservice",
+      title: "Finishing the api for full text parsing",
       id: "5",
       column: "todo",
     },
-    { title: "Postmortem for outage", id: "6", column: "todo" },
-    { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
-  
+    { title: "Optimizing extractFromText", id: "6", column: "todo" },
     // DOING
     {
-      title: "Refactor context providers to use Zustand",
+      title: "Implementing A Doc guideline for set ups and contributions",
       id: "8",
       column: "doing",
     },
-    { title: "Add logging to daily CRON", id: "9", column: "doing" },
+    { title: "Adding some utils for passing a multiple edgecases", id: "9", column: "doing" },
     // DONE
     {
-      title: "Set up DD dashboards for Lambda listener",
+      title: "Extract from a full markdown text",
       id: "10",
       column: "done",
     },
+    {
+        title: "Pruning the validation of the data",
+        id: "11",
+        column: "done",
+      },
   ];
